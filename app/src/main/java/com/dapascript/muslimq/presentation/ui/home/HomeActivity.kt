c3f3a7cf4f5dd70ebd321987ca7a6d0407dfbe14
dapoi <daffaprabowo5@gmail.com>
2022-08-09 18:49:21 +0700

    add some library & prepare architecture folder

--- app/src/main/java/com/dapascript/muslimq/presentation/ui/home/HomeActivity.kt
+++ app/src/main/java/com/dapascript/muslimq/presentation/ui/home/HomeActivity.kt
@@ -1,11 +1,12 @@
-package com.dapascript.muslimq
+package com.dapascript.muslimq.presentation.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dapascript.muslimq.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
