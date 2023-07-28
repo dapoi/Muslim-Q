9017c6f40c800124441884ef0fe9fe2fd259a5db
Luthfi Daffa Prabowo <luthfidaffa2202@gmail.com>
2023-07-28 09:48:40 +0700

    refactor whole notification code

---
+++ core/src/main/java/com/prodev/muslimq/core/utils/Constant.kt
@@ -0,0 +1,24 @@
+package com.prodev.muslimq.core.utils
+
+object Constant {
+    const val ADZAN_CODE = "adzan_code"
+    const val ADZAN_NAME = "adzan_name"
+    const val ADZAN_TIME = "adzan_time"
+    const val IS_SHUBUH = "is_shubuh"
+    const val KEY_ADZAN_SHUBUH = "Adzan Shubuh"
+    const val KEY_ADZAN_DZUHUR = "Adzan Dzuhur"
+    const val KEY_ADZAN_ASHAR = "Adzan Ashar"
+    const val KEY_ADZAN_MAGHRIB = "Adzan Maghrib"
+    const val KEY_ADZAN_ISYA = "Adzan Isya"
+    const val CHANNEL_ID_SHUBUH = "channel_shubuh"
+    const val CHANNEL_ID_DZUHUR = "channel_dzuhur"
+    const val CHANNEL_ID_ASHAR = "channel_ashar"
+    const val CHANNEL_ID_MAGHRIB = "channel_maghrib"
+    const val CHANNEL_ID_ISYA = "channel_isya"
+    const val CHANNEL_NAME_SHUBUH = "Shubuh"
+    const val CHANNEL_NAME_DZUHUR = "Dzuhur"
+    const val CHANNEL_NAME_ASHAR = "Ashar"
+    const val CHANNEL_NAME_MAGHRIB = "Maghrib"
+    const val CHANNEL_NAME_ISYA = "Isya"
+    const val STOP_ADZAN = "stop_adzan"
+}
