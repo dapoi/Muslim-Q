72ba9f4a42dc8b0b6ad53cd0806e9ff66255ae4c
Luthfi Daffa Prabowo <daffaprabowo5@gmail.com>
2023-08-24 12:51:04 +0700

    adjust end-point for shalat time

--- core/src/main/java/com/prodev/muslimq/core/data/repository/ShalatRepositoryImpl.kt
+++ core/src/main/java/com/prodev/muslimq/core/data/repository/ShalatRepositoryImpl.kt
@@ -14,6 +14,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import java.text.SimpleDateFormat
+import java.util.Calendar
 import java.util.Date
 import java.util.Locale
 import javax.inject.Inject
@@ -54,7 +55,10 @@ class ShalatRepositoryImpl @Inject constructor(
             dao.getShalatDailyByCity(city, country)
         },
         fetch = {
-            shalatService.getShalatDaily(city, country)
+            val calendar = Calendar.getInstance()
+            val currentYear = calendar.get(Calendar.YEAR)
+            val currentMonth = calendar.get(Calendar.MONTH) + 1
+            shalatService.getShalatDaily(currentYear, currentMonth, city, country)
         },
         saveFetchResult = { shalat ->
             val simpleDateFormat = SimpleDateFormat("dd MMM yyyy", Locale.getDefault())
--- core/src/main/java/com/prodev/muslimq/core/data/source/remote/network/ShalatApi.kt
+++ core/src/main/java/com/prodev/muslimq/core/data/source/remote/network/ShalatApi.kt
@@ -2,12 +2,15 @@ package com.prodev.muslimq.core.data.source.remote.network
 
 import com.prodev.muslimq.core.data.source.remote.model.ShalatResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ShalatApi {
 
-    @GET("v1/calendarByCity")
+    @GET("v1/calendarByCity/{year}/{month}")
     suspend fun getShalatDaily(
+        @Path("year") year: Int,
+        @Path("month") month: Int,
         @Query("city") city: String,
         @Query("country") country: String,
     ): ShalatResponse
